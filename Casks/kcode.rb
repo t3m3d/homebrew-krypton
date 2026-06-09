3ee8ba0088a24eee0628d4affbfb074f75710f23
t3m3d <brian@kryptonbytes.com>
2026-06-09 11:29:56 -0400

    kcode 0.3.0: refresh sha (git/minimap/md-preview/snippets/run)

--- Casks/kcode.rb
+++ Casks/kcode.rb
@@ -1,6 +1,6 @@
 cask "kcode" do
   version "0.3.0"
-  sha256 "a7bfc2a55f3c91029f9762ac3b56e1028ad93e0d0535d1eede9a9c270e987ca6"
+  sha256 "99876f6ec09dc5af08a207331356f6c82fa72d41de5441f6445b2ca2ac239e7f"
 
   url "https://github.com/t3m3d/kcode/releases/download/#{version}/kcode-#{version}-macos.app.zip"
   name "kcode"
