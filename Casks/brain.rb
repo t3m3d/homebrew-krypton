c5d8d4d1819fa9f2bf387a39913ef41afe1233d9
t3m3d <brian@kryptonbytes.com>
2026-06-10 19:24:56 -0400

    brain 0.9.2

--- Casks/brain.rb
+++ Casks/brain.rb
@@ -1,6 +1,6 @@
 cask "brain" do
-  version "0.9.1"
-  sha256 "9f4f567fb8ad1ed5c0cef87293773112890afdf2034db2763fcda7aa20e6d986"
+  version "0.9.2"
+  sha256 "8f7a784b6b7e06d29ef0dcb4300286e666a3273f316887d5e47204f313e13ae0"
 
   url "https://github.com/t3m3d/brain/releases/download/#{version}/brain-#{version}.dmg"
   name "brain"
