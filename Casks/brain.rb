79fa6a3099bc01933fd49aedcdd1fa7af483dc2d
t3m3d <brian@kryptonbytes.com>
2026-06-10 19:12:37 -0400

    brain cask -> t3m3d/brain repo releases

--- Casks/brain.rb
+++ Casks/brain.rb
@@ -2,7 +2,7 @@ cask "brain" do
   version "0.9.1"
   sha256 "9f4f567fb8ad1ed5c0cef87293773112890afdf2034db2763fcda7aa20e6d986"
 
-  url "https://github.com/t3m3d/krypton/releases/download/brain-#{version}/brain-#{version}.dmg"
+  url "https://github.com/t3m3d/brain/releases/download/#{version}/brain-#{version}.dmg"
   name "brain"
   desc "Pure-Krypton macOS IDE (editor, tree, tabs, live highlighting) on the objk FFI"
   homepage "https://krypton-lang.org/programs/brain.html"
