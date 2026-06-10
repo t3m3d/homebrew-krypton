f92c738ef2b3c6838c21add883adbc35ab14e4b4
t3m3d <brian@kryptonbytes.com>
2026-06-10 01:31:22 -0400

    Add brain cask 0.1.0

---
+++ Casks/brain.rb
@@ -0,0 +1,17 @@
+cask "brain" do
+  version "0.1.0"
+  sha256 "6c2e7a3848310839572cde9098134881ab783237fb3cf926111d9c3d3366a776"
+
+  url "https://github.com/t3m3d/krypton/releases/download/brain-#{version}/brain-#{version}.dmg"
+  name "brain"
+  desc "Pure-Krypton macOS IDE (editor, tree, tabs, live highlighting) on the objk FFI"
+  homepage "https://krypton-lang.org/programs/brain.html"
+
+  depends_on arch: :arm64
+
+  app "brain.app"
+
+  postflight do
+    system_command "/usr/bin/xattr", args: ["-dr", "com.apple.quarantine", "/Applications/brain.app"]
+  end
+end
