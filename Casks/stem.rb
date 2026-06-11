bc0c1375de320267ebd0ec26f43bf5ad86ef3198
t3m3d <brian@kryptonbytes.com>
2026-06-11 11:57:35 -0400

    stem 0.12.1

--- Casks/stem.rb
+++ Casks/stem.rb
@@ -1,6 +1,6 @@
 cask "stem" do
-  version "0.12.0"
-  sha256 "2cabae0d7d5e123e8ed90820fc16c98c2b45871ea7875c1c975e0b37f77a56ac"
+  version "0.12.1"
+  sha256 "a53e17180dcbb9af10cae0ded4f70baa9f97f9cebdd6a7d4e01a03cbd10d816f"
 
   url "https://github.com/t3m3d/stem/releases/download/#{version}/stem-#{version}.dmg"
   name "stem"
